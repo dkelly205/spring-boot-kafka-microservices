133828a6a0766066575564c93fe1648ea78b0227
dkelly205 <danieljkelly91@gmail.com>
2025-04-06 18:54:31 +0100

    Creates microservices

---
+++ base-domains/src/main/java/com/dkelly205/base_domains/dto/OrderEvent.java
@@ -0,0 +1,17 @@
+package com.dkelly205.base_domains.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderEvent {
+
+    private String message;
+    private String status;
+    private Order order;
+}
---
+++ base-domains/src/test/java/com/dkelly205/base_domains/BaseDomainsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dkelly205.base_domains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseDomainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
